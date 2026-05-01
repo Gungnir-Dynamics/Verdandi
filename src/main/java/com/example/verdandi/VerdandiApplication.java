e5841ab765742482da7947f5b1fa3cecb2e860e4
KasperJChristensen <Kasperj2016@gmail.com>
2026-05-01 15:14:14 +0200

    test

---
+++ src/main/java/com/example/verdandi/VerdandiApplication.java
@@ -0,0 +1,13 @@
+package com.example.verdandi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerdandiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerdandiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/verdandi/VerdandiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.verdandi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerdandiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
