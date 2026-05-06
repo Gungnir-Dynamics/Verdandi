0227429186bb8331d381a719507c97300d381fb6
KasperJChristensen <Kasperj2016@gmail.com>
2026-05-06 09:26:42 +0200

    Test opstart

---
+++ src/test/java/com/example/verdandi/repository/ProjectRepoTest.java
@@ -0,0 +1,12 @@
+package com.example.verdandi.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectRepoTest {
+
+    @Test
+    void getProjects() {
+    }
+}
