5a009997d295d7eef5e017a28ac7afe0adf47ae6
Tor1011 <tor@taranta.dk>
2026-05-07 21:23:47 +0200

    fikset getMultipleProjects

--- src/main/java/com/example/verdandi/service/ProjectService.java
+++ src/main/java/com/example/verdandi/service/ProjectService.java
@@ -1,9 +1,8 @@
 package com.example.verdandi.service;
 
+import com.example.verdandi.exception.DatabaseOperationException;
 import com.example.verdandi.model.Project;
-import com.example.verdandi.model.SubProject;
 import com.example.verdandi.repository.ProjectRepo;
-import com.example.verdandi.repository.SubProjectRepo;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
@@ -12,31 +11,24 @@ import java.util.List;
 @Service
 public class ProjectService {
     private final ProjectRepo projectRepo;
-    private final SubProjectRepo subProjectRepo;
 
-    public ProjectService (ProjectRepo projectRepo, SubProjectRepo subProjectRepo){
+    public ProjectService(ProjectRepo projectRepo) {
         this.projectRepo = projectRepo;
-        this.subProjectRepo = subProjectRepo;
     }
 
-    public List<Project> getMultipleProjects(){
-        return projectRepo.getMultipleProjects();
+    public List<Project> getMultipleProjects() {
+        try {
+            return projectRepo.getMultipleProjects();
+        } catch (DataAccessException ex) {
+            throw new DatabaseOperationException("Failed to retrieve data for project", ex);
+        }
     }
 
-    public Project getSingleProject(int projectId){
+    public Project getSingleProject(int projectId) {
         return projectRepo.getSingleProject(projectId);
     }
 
-    public void saveProject(Project project){
+    public void saveProject(Project project) {
         projectRepo.createProject(project);
-    }
-}
-//     public List<Project>getProjects(){
-//         try {
-//             return projectRepo.getProjects();
-//         } catch (DataAccessException ex) {
-//             throw new DatabaseOperationException("Failed to retrieve data for project", ex); {
-//             }
-//         }
     }
 }
