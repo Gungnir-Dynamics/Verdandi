6db3b6197192cc2189279831d1d8c654525e754a
KasperJChristensen <Kasperj2016@gmail.com>
2026-05-11 15:32:46 +0200

    edit og save virker nu med ValidationException

--- src/main/java/com/example/verdandi/controller/ProjectController.java
+++ src/main/java/com/example/verdandi/controller/ProjectController.java
@@ -6,7 +6,6 @@ import com.example.verdandi.model.Project;
 import com.example.verdandi.service.ProjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
