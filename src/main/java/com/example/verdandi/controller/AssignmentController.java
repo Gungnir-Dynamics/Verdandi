3322cfbc2ef764fe888cdf4a4bc504fd132543cf
Tor1011 <tor@taranta.dk>
2026-05-21 00:35:30 +0200

    test

--- src/main/java/com/example/verdandi/controller/AssignmentController.java
+++ src/main/java/com/example/verdandi/controller/AssignmentController.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-//
+
 @Controller
 @RequestMapping("/projects/{projectId}/assignments")
 public class AssignmentController {
